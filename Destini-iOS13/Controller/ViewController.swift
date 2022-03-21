17378c28753d0ca0e88e40a1dffcc6abdebcb0fd
dbunk903 <dbunk903@gmail.com>
2022-03-21 15:51:36 +0900

    Story -> Stories array

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -11,15 +11,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let example = Story(t: "You see a fork in the road.",
-                        c1: "Take a left.",
-                        c2: "Take a right.")
+    let stories = [Story(t: "You see a fork in the road.",
+                         c1: "Take a left.",
+                         c2: "Take a right."),
+                   Story(t: "you see a tiger", c1: "Shout for help", c2: "Play Dead"),
+
+                   Story(t: "You find a Treasure Chest", c1: "Open it", c2: "Check for Traps")]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = example.title
-        choice1Button.setTitle(example.choice1, for: UIControl.State.normal)
-        choice2Button.setTitle(example.choice2, for: UIControl.State.normal)
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: UIControl.State.normal)
+        choice2Button.setTitle(stories[0].choice2, for: UIControl.State.normal)
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
