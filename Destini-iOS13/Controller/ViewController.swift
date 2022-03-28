bd3477b38ed16574ee5d6b6d835c4ee155f3990e
dbunk903 <dbunk903@gmail.com>
2022-03-28 14:12:16 +0900

    22.3.28

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -11,9 +11,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let stories = [Story(t: "You see a fork in the road.",
-                         c1: "Take a left.",
-                         c2: "Take a right."),
+    let stories = [Story(t: "You see a fork in the road.",c1: "Take a left.",c2: "Take a right."),
                    Story(t: "you see a tiger", c1: "Shout for help", c2: "Play Dead"),
                    Story(t: "You find a Treasure Chest", c1: "Open it", c2: "Check for Traps")]
     
