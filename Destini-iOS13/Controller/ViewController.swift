9f39609ba10c4f187efb2fddcbcfcdfd37b9243c
dbunk903 <dbunk903@gmail.com>
2022-04-01 12:25:59 +0900

    Complete

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,29 +10,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    let sb = StoryBrain()
+    
+    var sb = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = sb.stories[0].title
-        choice1Button.setTitle(sb.stories[0].choice1, for: UIControl.State.normal)
-        choice2Button.setTitle(sb.stories[0].choice2, for: UIControl.State.normal)
+        updateUI()
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-        let userChoice = sender.currentTitle!
-        if userChoice == sb.stories[0].choice1{
-            storyLabel.text = sb.stories[1].title
-            choice1Button.setTitle(sb.stories[1].choice1, for: UIControl.State.normal)
-            choice2Button.setTitle(sb.stories[1].choice2, for: UIControl.State.normal)
-        }
-        else{
-            storyLabel.text = sb.stories[2].title
-            choice1Button.setTitle(sb.stories[2].choice1, for: UIControl.State.normal)
-            choice2Button.setTitle(sb.stories[2].choice2, for: UIControl.State.normal)
-        }
+        sb.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
+        
     }
     
+    func updateUI(){
+        storyLabel.text = sb.getStoryTitle()
+        choice1Button.setTitle(sb.getChoice1(), for: .normal)
+        choice2Button.setTitle(sb.getChoice2(), for: .normal)
+    }
 }
 
 
